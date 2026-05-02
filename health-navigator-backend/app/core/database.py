24d922bbe228b5e6e418e29c44cf2e34782daf07
JiunShin1214 <jiunsin15@naver.com>
2026-05-03 01:26:03 +0900

    achore(db): update database connection settings
    - Change DB user from root to hn_user
    - Disable SQLAlchemy echo logging

--- health-navigator-backend/app/core/database.py
+++ health-navigator-backend/app/core/database.py
@@ -3,12 +3,11 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # MySQL 연결 URL
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:jiun7925!@localhost:3306/health_navigator"
-
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://hn_user:1234@localhost:3306/health_navigator"
 # 엔진 생성
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    echo=True  # SQL 로그 확인용 (나중에 False로 변경 가능)
+    echo=False  # SQL 로그 확인용 (나중에 False로 변경 가능)
 )
 
 # 세션 생성
